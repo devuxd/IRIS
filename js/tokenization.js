3d67705142528c83f185113b88ef6cb2903f3b39
Efe Ozturkoglu <efeecehanozturkoglu@yahoo.com>
2018-06-07 16:40:08 -0400

    Created objects for tokenized lines of code and tokenized files

--- js/tokenization.js
+++ js/tokenization.js
@@ -1,4 +1,4 @@
-{
+tokenizdHTML {
 	lines: [
 		[{token}, {token}, {token}]
 	]
@@ -20,11 +20,40 @@ tokenTypes = Object.freeze({
 /**
  * Creates a 'token' object which is structured as follows:
  *   token: { type: tokenTypes, value: string }
- * @param tokenType The type of the HTML token as defined by the tokenTypes enum
- * @param tokenValue The plain text of the original token from the HTML
+ * @param tokenType The type of the HTML token as defined by the tokenTypes
+ *   enum.
+ * @param tokenValue The plain text of the original token from the HTML. If a
+ *   token is created to add extra information, it should have an empty string
+ *   as its value.
  * @returns Initialized token object
  */
 function token(tokenType, tokenValue) {
 	this.type = tokenType;
 	this.value = tokenValue;
+}
+
+
+/**
+ * Creates a 'coeLine' object representing a line of code broken up into tokens.
+ */
+function codeLine() {
+	// Array of 'token' objects
+	this.tokens = [];
+}
+
+/**
+ * Adds a 'token' to the end of the line
+ */
+codeLine.prototype.addToken = function(token) {
+	this.tokens.push(token);
+};
+
+
+function tokenizedHTML() {
+	// Array of 'codeLine' objects
+	this.lines = [];
+}
+
+tokenizedHTML.prototype.addLine = function() {
+	this.lines.push(new codeLine());
 }
