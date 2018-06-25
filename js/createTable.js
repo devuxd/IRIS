89dc5afd0a4762d60395be01502f07ba31347c71
Andrea Solis <39932150+SolAndrea@users.noreply.github.com>
2018-06-24 23:25:37 -0400

    Update createTable.js

--- js/createTable.js
+++ js/createTable.js
@@ -86,6 +86,22 @@ function createTable() {
 	}
 }
 
+//TO DO: Create function that populates training set from new tokenizer
+/**
+*Build desicion tree model, and stores prediction for next element
+* training: training set, array of the elements' features.
+* 	->[{'tag':'div', 'attrKey':'class', 'parentTag':'div', 'parentAttr/Val':'class=home_banner', 'value':'sample_button'},...];
+* samples: current element's features, used to predict the value of the class
+*   ->{'tag':'h2', 'attrKey':'class', 'parentTag':'', 'parentAttr/Val':''};
+*/
+function id3tree(){
+	if (training.length != 0){
+		document.tree = id3(training,'value',features);
+	}
+	if ( typeof (document.tree) != "undefined" && typeof (samples) != "undefined"){
+		predictions = predict(document.tree, samples);
+	}
+}
 
 /**
  * Organizes each element in a table format to keep track of the frequency of
