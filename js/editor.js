c515c6597e1a02335378999f194a5e5cecf320de
Kartik <kartikchugh.kc@gmail.com>
2018-08-06 00:31:43 -0400

    Fix key bug

--- js/editor.js
+++ js/editor.js
@@ -96,6 +96,7 @@ $(document).ready(function() {
         aceEditor.on('focus', function (event, editors) {
             $(this).keyup(function (e) {
                 if (aceEditor.isFocused()) {
+                    if (e.key === 'Control' && prevKey === 'Shift') aceEditor.onPaste = function(text, event) { this.commands.exec("paste", this, {text: text, event: event});};
                     handleKey(e.key, aceEditor, outputFrame);
                     if (((e.key.length === 1 && /[\w"'< ]/.test(e.key)) || e.key === ',' && prevKey === 'Shift') && storage.predictionCase !== PREDICTION_CASE.NONE) {
                         aceEditor.commands.byName.startAutocomplete.exec(aceEditor);
